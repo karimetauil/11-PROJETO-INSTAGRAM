cbe14dd49ca2fe2468f5b0ede88fc93f08c003a1
karimetauil <karimeupd@hotmail.com>
2023-05-30 15:31:24 -0300

    alt script

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+
 let imagem = document.getElementById("troca-imagem")
 
-    imagem.style.opacity = 0
-
-        function trocaImagem(){
-            console.log("TO DENTRO DA FUNÇÃO")
+        function trocaImagem() {
+            if (imagem.style.opacity == 0) {
+            imagem.style.opacity = 1
+            }
+            else {
+                imagem.style.opacity = 0
+            }
+        }
 
 
   setInterval(() => {
-    
-  }, interval);
+    trocaImagem()
+  }, 4000);
